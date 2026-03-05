fc75460cfc8ae63a06149553e5bb1ea984548d31
govvnn <dayne.gov.vn@gmail.com>
2026-03-05 23:20:07 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.getElementById("playBtn").onclick=function(){
+alert("Thêm link Roblox game của bạn vào đây!");
+}
+
+document.getElementById("playBtn2").onclick=function(){
+alert("Thêm link Roblox game của bạn vào đây!");
+}
